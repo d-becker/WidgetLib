ef105a78edd6880c22d9932ba40fe43504a98d77
Daniel Becker <beckerdaniel.dani@gmail.com>
2016-04-11 20:12:05 +0200

    Also changed class Toplevel.

--- include/Toplevel.hpp
+++ include/Toplevel.hpp
@@ -1,7 +1,6 @@
 #ifndef TOPLEVEL_H
 #define TOPLEVEL_H
 
-#include <memory>
 #include "Container.hpp"
 
 namespace wl {
@@ -9,17 +8,16 @@ namespace wl {
 class Toplevel : public Container
 {
 public:
-  Toplevel(std::shared_ptr<Container> parent,
-	   Vec2 position = Vec2(0, 0),
+  Toplevel(Vec2 position = Vec2(0, 0),
 	   int width = 0,
 	   int height = 0);
 
   virtual ~Toplevel();
 
 private:
-  std::shared_ptr<Widget> m_mouse_btn_left_pressed;
-  std::shared_ptr<Widget> m_mouse_btn_right_pressed;
-  std::shared_ptr<Widget> m_mouse_inside;
+  Widget *m_mouse_btn_left_pressed;
+  Widget *m_mouse_btn_right_pressed;
+  Widget *m_mouse_inside;
 };
 
 } // namespace wl
--- src/Toplevel.cpp
+++ src/Toplevel.cpp
@@ -2,11 +2,10 @@
 
 namespace wl {
 
-Toplevel::Toplevel(std::shared_ptr<Container> parent,
-		   Vec2 position = Vec2(0, 0),
-		   int width = 0,
-		   int height = 0)
-  : Container(parent, position, width, height),
+Toplevel::Toplevel(Vec2 position,
+		   int width,
+		   int height)
+  : Container(nullptr, position, width, height),
     m_mouse_btn_left_pressed(nullptr),
     m_mouse_btn_right_pressed(nullptr),
     m_mouse_inside(nullptr)
